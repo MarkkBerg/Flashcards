bb13ceb131ff0428c8e7b6f95444127afbe46d4c
MarkkBerg <72448257+MarkkBerg@users.noreply.github.com>
2026-05-21 12:22:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'flashcards-v' + Date.now();
+const FILES = ['/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+// Network first : toujours essayer le réseau avant le cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
